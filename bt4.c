134f84e0776baacb2be9994e48c61269de849910
gklwong <gklwong@gmail.com>
2020-03-30 00:42:06 +0800

    30/3

---
+++ bt4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main(void)
+{
+    float a,b,c;
+
+    printf("Solving quadratic equation: ax^2 +bx +c\n");
+
+    //your code...
+
+    system("pause");
+    return 0;
+}
---
+++ bt5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    float a,b;
+    char op;
+
+    printf("Enter a simple math expression with two operands (Example: 3 + 2)\nSeparate them with a space\n");
+    printf("(Operator supported: +  -  *  / ^)\n\n");
+
+    //you code here...
+
+    system("pause");
+    return 0;
+}
